f79511dc7a7a2f4d671dd49bb7de40bccc59a940
Tushar Tari <tushartari11@gmail.com>
2025-03-29 20:40:19 +0100

    code changes for admin dashboard

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from '../services/admin.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss',
 })
-export class AdminDashboardComponent {}
+export class AdminDashboardComponent implements OnInit {
+  user_dashboard_data: any;
+  total_user: number = 0;
+  admin_user: number = 0;
+  seller_user: number = 0;
+  buyer_user: number = 0;
+
+  product_dashboard_data: any;
+  total_product: number = 0;
+  publish_product: number = 0;
+  inactive_product: number = 0;
+  draft_product: number = 0;
+
+  constructor(private router: Router, private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.adminProductDashboardData();
+    this.adminUserDashboardData();
+  }
+
+  userDashboard() {
+    this.router.navigateByUrl('/admin/user');
+  }
+
+  productDashboard() {
+    this.router.navigateByUrl('/admin/product');
+  }
+
+  adminUserDashboardData() {
+    this.adminService.userDashboardData().subscribe(
+      (data) => {
+        this.user_dashboard_data = data;
+        console.log('User Dashboard Data:', this.user_dashboard_data);
+        for (let user in this.user_dashboard_data) {
+          if (this.user_dashboard_data[user].role == 'admin') {
+            this.admin_user++;
+          } else if (this.user_dashboard_data[user].role == 'seller') {
+            this.seller_user++;
+          } else if (this.user_dashboard_data[user].role == 'buyer') {
+            this.buyer_user++;
+          }
+          this.total_user++;
+        }
+      },
+      (error) => {
+        console.log('Error fetching user dashboard data:', error);
+      }
+    );
+  }
+
+  adminProductDashboardData() {
+    this.adminService.productDashboardData().subscribe(
+      (data) => {
+        this.product_dashboard_data = data;
+        console.log('User Dashboard Data:', this.product_dashboard_data);
+        for (let product in this.product_dashboard_data) {
+          if (this.product_dashboard_data[product].status == 'publish') {
+            this.publish_product++;
+          } else if (
+            this.product_dashboard_data[product].status == 'inactive'
+          ) {
+            this.inactive_product++;
+          } else if (this.product_dashboard_data[product].status == 'draft') {
+            this.draft_product++;
+          }
+          this.total_product++;
+        }
+      },
+      (error) => {
+        console.log('Error fetching product dashboard data:', error);
+      }
+    );
+  }
+
+  logout() {
+    sessionStorage.removeItem('user_session_id');
+    sessionStorage.removeItem('role');
+  }
+}
--- src/app/admin/admin-login/admin-login.component.ts
+++ src/app/admin/admin-login/admin-login.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginSignupService } from '../../shared/services/login-signup.service';
 
 @Component({
   selector: 'app-admin-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './admin-login.component.html',
-  styleUrl: './admin-login.component.scss'
+  styleUrl: './admin-login.component.scss',
 })
-export class AdminLoginComponent {
+export class AdminLoginComponent implements OnInit {
+  signInFormValue: any = {};
+  user_data: any;
 
+  constructor(
+    private router: Router,
+    private loginService: LoginSignupService
+  ) {}
+
+  ngOnInit(): void {}
+
+  onSubmitSignIn() {
+    this.loginService
+      .adminLogin(
+        this.signInFormValue.userEmail,
+        this.signInFormValue.userPassword
+      )
+      .subscribe(
+        (data) => {
+          this.user_data = data;
+          if (this.user_data.length == 1) {
+            sessionStorage.setItem(
+              'user_session_id',
+              this.user_data[0].user_id
+            );
+            sessionStorage.setItem('role', this.user_data[0].role);
+            this.router.navigateByUrl('/admin-dashboard');
+          } else {
+            alert('Invalid Response');
+          }
+          console.log('Login successful:', data);
+        },
+        (error) => {
+          console.log('Error during login:', error);
+          alert('An error occurred during login. Please try again.');
+        }
+      );
+  }
 }
